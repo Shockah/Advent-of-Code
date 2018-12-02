8ad0922a0ba4f6bc8197262670ca8a52bec163ac
Michał Dolaś <me@shockah.pl>
2018-12-02 21:23:48 +0100

    Y17D1 tests

--- src/pl/shockah/aoc/y2017/Day1.kt
+++ src/pl/shockah/aoc/y2017/Day1.kt
@@ -1,5 +1,8 @@
 package pl.shockah.aoc.y2017
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import pl.shockah.aoc.AdventTask
 import kotlin.streams.toList
 
@@ -32,5 +35,70 @@ class Day1 : AdventTask<List<Int>, Int, Int>(2017, 1) {
 
 	override fun part2(input: List<Int>): Int {
 		return task(input, Mode.HalfwayThrough)
+	}
+
+	@Suppress("FunctionName")
+	class Tests {
+		private val task = Day1()
+
+		@Nested
+		inner class Part1 {
+			@Test
+			fun `#1`() {
+				val input = task.parseInput("1122")
+				Assertions.assertEquals(3, task.part1(input))
+			}
+
+			@Test
+			fun `#2`() {
+				val input = task.parseInput("1111")
+				Assertions.assertEquals(4, task.part1(input))
+			}
+
+			@Test
+			fun `#3`() {
+				val input = task.parseInput("1234")
+				Assertions.assertEquals(0, task.part1(input))
+			}
+
+			@Test
+			fun `#4`() {
+				val input = task.parseInput("91212129")
+				Assertions.assertEquals(9, task.part1(input))
+			}
+		}
+
+		@Nested
+		inner class Part2 {
+			@Test
+			fun `#1`() {
+				val input = task.parseInput("1212")
+				Assertions.assertEquals(6, task.part2(input))
+			}
+
+			@Test
+			fun `#2`() {
+				val input = task.parseInput("1221")
+				Assertions.assertEquals(0, task.part2(input))
+			}
+
+			@Test
+			fun `#3`() {
+				val input = task.parseInput("123425")
+				Assertions.assertEquals(4, task.part2(input))
+			}
+
+			@Test
+			fun `#4`() {
+				val input = task.parseInput("123123")
+				Assertions.assertEquals(12, task.part2(input))
+			}
+
+			@Test
+			fun `#5`() {
+				val input = task.parseInput("12131415")
+				Assertions.assertEquals(4, task.part2(input))
+			}
+		}
 	}
 }
