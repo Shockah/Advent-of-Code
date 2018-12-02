96bf9b219df2d8edff9ba0c8e6ef2eecb9e29226
Michał Dolaś <me@shockah.pl>
2018-12-02 21:21:21 +0100

    Y15D3 tests

--- src/pl/shockah/aoc/y2015/Day3.kt
+++ src/pl/shockah/aoc/y2015/Day3.kt
@@ -1,5 +1,8 @@
 package pl.shockah.aoc.y2015
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import pl.shockah.aoc.AdventTask
 
 class Day3 : AdventTask<List<Day3.Direction>, Int, Int>(2015, 3) {
@@ -41,5 +44,52 @@ class Day3 : AdventTask<List<Day3.Direction>, Int, Int>(2015, 3) {
 
 	override fun part2(input: List<Direction>): Int {
 		return task(input, 2)
+	}
+
+	@Suppress("FunctionName")
+	class Tests {
+		private val task = Day3()
+
+		@Nested
+		inner class Part1 {
+			@Test
+			fun `#1`() {
+				val input = task.parseInput(">")
+				Assertions.assertEquals(2, task.part1(input))
+			}
+
+			@Test
+			fun `#2`() {
+				val input = task.parseInput("^>v<")
+				Assertions.assertEquals(4, task.part1(input))
+			}
+
+			@Test
+			fun `#3`() {
+				val input = task.parseInput("^v^v^v^v^v")
+				Assertions.assertEquals(2, task.part1(input))
+			}
+		}
+
+		@Nested
+		inner class Part2 {
+			@Test
+			fun `#1`() {
+				val input = task.parseInput("^v")
+				Assertions.assertEquals(3, task.part2(input))
+			}
+
+			@Test
+			fun `#2`() {
+				val input = task.parseInput("^>v<")
+				Assertions.assertEquals(3, task.part2(input))
+			}
+
+			@Test
+			fun `#3`() {
+				val input = task.parseInput("^v^v^v^v^v")
+				Assertions.assertEquals(11, task.part2(input))
+			}
+		}
 	}
 }
