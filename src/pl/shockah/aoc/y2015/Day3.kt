dd485bc14e22fe22eb288d1e3551e7eade79c249
Michał Dolaś <me@shockah.pl>
2018-12-02 15:09:32 +0100

    Y15D3

---
+++ src/pl/shockah/aoc/y2015/Day3.kt
@@ -0,0 +1,46 @@
+package pl.shockah.aoc.y2015
+
+import pl.shockah.aoc.AdventTask
+import java.io.File
+
+class Day3 : AdventTask<List<Day3.Direction>, Int, Int>(2015, 3) {
+	enum class Direction(
+			val symbol: Char,
+			val x: Int,
+			val y: Int
+	) {
+		Right('>', 1, 0),
+		Up('^', 0, -1),
+		Left('<', -1, 0),
+		Down('v', 0, 1)
+	}
+
+	override fun parseInput(file: File): List<Direction> {
+		return file.readText().toCharArray().map { char -> Direction.values().first { it.symbol == char } }
+	}
+
+	private fun task(input: List<Direction>, santaCount: Int = 1): Int {
+		val santas = Array(santaCount) { Pair(0, 0) }
+		val visited = mutableSetOf(Pair(0, 0))
+		var santaIndex = 0
+
+		for (direction in input) {
+			santas[santaIndex] = Pair(
+					santas[santaIndex].first + direction.x,
+					santas[santaIndex].second + direction.y
+			)
+			visited += santas[santaIndex]
+			santaIndex = (santaIndex + 1) % santaCount
+		}
+
+		return visited.size
+	}
+
+	override fun part1(input: List<Direction>): Int {
+		return task(input, 1)
+	}
+
+	override fun part2(input: List<Direction>): Int {
+		return task(input, 2)
+	}
+}
