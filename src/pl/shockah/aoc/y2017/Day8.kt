88242ac222d0e789cf2e72482180245506091d36
Michał Dolaś <me@shockah.pl>
2018-12-02 12:02:02 +0100

    Y17D8

--- src/pl/shockah/aoc/y2017/Day8.kt
+++ src/pl/shockah/aoc/y2017/Day8.kt
@@ -3,6 +3,7 @@ package pl.shockah.aoc.y2017
 import pl.shockah.aoc.AdventTask
 import java.io.File
 import java.util.regex.Pattern
+import kotlin.math.max
 
 class Day8: AdventTask<List<Day8.Instruction>, Int, Int>(2017, 8) {
 	private val inputPattern: Pattern = Pattern.compile("(\\w+) ((?:inc)|(?:dec)) (-?\\d+) if (\\w+) ((?:==)|(?:!=)|(?:>)|(?:<)|(?:>=)|(?:<=)) (-?\\d+)")
@@ -46,16 +47,33 @@ class Day8: AdventTask<List<Day8.Instruction>, Int, Int>(2017, 8) {
 		}
 	}
 
-	override fun part1(input: List<Instruction>): Int {
+	enum class Mode {
+		FinalMaxValue, MaxValueEver
+	}
+
+	@Suppress("NOTHING_TO_INLINE")
+	private inline fun task(input: List<Instruction>, mode: Mode): Int {
 		val registers = mutableMapOf<String, Int>()
+		var maxValue = 0
 		for (instruction in input) {
-			if (instruction.conditionOperator(registers.computeIfAbsent(instruction.conditionRegister) { 0 }, instruction.conditionValue))
+			if (instruction.conditionOperator(registers.computeIfAbsent(instruction.conditionRegister) { 0 }, instruction.conditionValue)) {
 				registers[instruction.register] = registers.computeIfAbsent(instruction.register) { 0 } + instruction.adding
+				if (mode == Mode.MaxValueEver)
+					maxValue = max(maxValue, registers[instruction.register]!!)
+			}
 		}
-		return registers.values.max()!!
+
+		return when (mode) {
+			Mode.FinalMaxValue -> registers.values.max()!!
+			Mode.MaxValueEver -> maxValue
+		}
+	}
+
+	override fun part1(input: List<Instruction>): Int {
+		return task(input, Mode.FinalMaxValue)
 	}
 
 	override fun part2(input: List<Instruction>): Int {
-		TODO()
+		return task(input, Mode.MaxValueEver)
 	}
 }
