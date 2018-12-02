e02a458830a52bf8b1e2fcb31585d680da05a394
Michał Dolaś <me@shockah.pl>
2018-12-02 21:33:33 +0100

    Y17D4 tests

--- src/pl/shockah/aoc/y2017/Day4.kt
+++ src/pl/shockah/aoc/y2017/Day4.kt
@@ -1,5 +1,8 @@
 package pl.shockah.aoc.y2017
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import pl.shockah.aoc.AdventTask
 
 class Day4: AdventTask<List<List<String>>, Int, Int>(2017, 4) {
@@ -21,5 +24,64 @@ class Day4: AdventTask<List<List<String>>, Int, Int>(2017, 4) {
 				it.toCharArray().sortedArray().contentToString()
 			}.toTypedArray()).size == it.size
 		}.size
+	}
+
+	@Suppress("FunctionName")
+	class Tests {
+		private val task = Day4()
+
+		@Nested
+		inner class Part1 {
+			@Test
+			fun `#1`() {
+				val input = task.parseInput("aa bb cc dd ee")
+				Assertions.assertEquals(1, task.part1(input))
+			}
+
+			@Test
+			fun `#2`() {
+				val input = task.parseInput("aa bb cc dd aa")
+				Assertions.assertEquals(0, task.part1(input))
+			}
+
+			@Test
+			fun `#3`() {
+				val input = task.parseInput("aa bb cc dd aaa")
+				Assertions.assertEquals(1, task.part1(input))
+			}
+		}
+
+		@Nested
+		inner class Part2 {
+			@Test
+			fun `#1`() {
+				val input = task.parseInput("abcde fghij")
+				Assertions.assertEquals(1, task.part2(input))
+			}
+
+			@Test
+			fun `#2`() {
+				val input = task.parseInput("abcde xyz ecdab")
+				Assertions.assertEquals(0, task.part2(input))
+			}
+
+			@Test
+			fun `#3`() {
+				val input = task.parseInput("a ab abc abd abf abj")
+				Assertions.assertEquals(1, task.part2(input))
+			}
+
+			@Test
+			fun `#4`() {
+				val input = task.parseInput("iiii oiii ooii oooi oooo")
+				Assertions.assertEquals(1, task.part2(input))
+			}
+
+			@Test
+			fun `#5`() {
+				val input = task.parseInput("oiii ioii iioi iiio")
+				Assertions.assertEquals(0, task.part2(input))
+			}
+		}
 	}
 }
