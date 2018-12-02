51ec64e9e661658f1fce11d6900a82daee32c1b1
Michał Dolaś <me@shockah.pl>
2018-12-02 21:12:49 +0100

    Y15D1 tests

--- src/pl/shockah/aoc/y2015/Day1.kt
+++ src/pl/shockah/aoc/y2015/Day1.kt
@@ -1,5 +1,8 @@
 package pl.shockah.aoc.y2015
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import pl.shockah.aoc.AdventTask
 
 class Day1 : AdventTask<String, Int, Int>(2015, 1) {
@@ -19,5 +22,82 @@ class Day1 : AdventTask<String, Int, Int>(2015, 1) {
 				return i + 1
 		}
 		throw IllegalArgumentException("Never reaching basement.")
+	}
+
+	@Suppress("FunctionName")
+	class Tests {
+		private val task = Day1()
+
+		@Nested
+		inner class Part1 {
+			@Test
+			fun `#1`() {
+				val input = task.parseInput("(())")
+				Assertions.assertEquals(0, task.part1(input))
+			}
+
+			@Test
+			fun `#2`() {
+				val input = task.parseInput("()()")
+				Assertions.assertEquals(0, task.part1(input))
+			}
+
+			@Test
+			fun `#3`() {
+				val input = task.parseInput("(((")
+				Assertions.assertEquals(3, task.part1(input))
+			}
+
+			@Test
+			fun `#4`() {
+				val input = task.parseInput("(()(()(")
+				Assertions.assertEquals(3, task.part1(input))
+			}
+
+			@Test
+			fun `#5`() {
+				val input = task.parseInput("))(((((")
+				Assertions.assertEquals(3, task.part1(input))
+			}
+
+			@Test
+			fun `#6`() {
+				val input = task.parseInput("())")
+				Assertions.assertEquals(-1, task.part1(input))
+			}
+
+			@Test
+			fun `#7`() {
+				val input = task.parseInput("))(")
+				Assertions.assertEquals(-1, task.part1(input))
+			}
+
+			@Test
+			fun `#8`() {
+				val input = task.parseInput(")))")
+				Assertions.assertEquals(-3, task.part1(input))
+			}
+
+			@Test
+			fun `#9`() {
+				val input = task.parseInput(")())())")
+				Assertions.assertEquals(-3, task.part1(input))
+			}
+		}
+
+		@Nested
+		inner class Part2 {
+			@Test
+			fun `#1`() {
+				val input = task.parseInput(")")
+				Assertions.assertEquals(1, task.part2(input))
+			}
+
+			@Test
+			fun `#2`() {
+				val input = task.parseInput("()())")
+				Assertions.assertEquals(5, task.part2(input))
+			}
+		}
 	}
 }
