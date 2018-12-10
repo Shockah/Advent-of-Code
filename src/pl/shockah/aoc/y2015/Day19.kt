42c6467cd3f6cc6bc8c6faa5e7ffa40488ec0fb2
Michał Dolaś <me@shockah.pl>
2018-12-10 15:42:07 +0100

    Y15D19

--- src/pl/shockah/aoc/y2015/Day19.kt
+++ src/pl/shockah/aoc/y2015/Day19.kt
@@ -13,6 +13,25 @@ import java.util.regex.Pattern
 private val String.moleculeLength: Int
 	get() = count { it.isUpperCase() || it == 'e' }
 
+private fun String.replace(startIndex: Int, endIndex: Int, replacement: String): String {
+	val builder = StringBuilder(this)
+	builder.replace(startIndex, endIndex, replacement)
+	return builder.toString()
+}
+
+private fun String.allIndexesOf(find: String): List<Int> {
+	val results = mutableListOf<Int>()
+	var currentIndex = 0
+	while (true) {
+		val foundAt = indexOf(find, currentIndex)
+		if (foundAt == -1)
+			return results
+
+		results += foundAt
+		currentIndex = foundAt + find.length
+	}
+}
+
 class Day19 : AdventTask<Day19.Input, Int, Int>(2015, 19) {
 	private val replacementInputPattern: Pattern = Pattern.compile("(\\w+) => (\\w+)")
 
@@ -38,12 +57,8 @@ class Day19 : AdventTask<Day19.Input, Int, Int>(2015, 19) {
 	private fun getReplacements(input: String, replacements: List<Pair<String, String>>): Set<String> {
 		val results = mutableSetOf<String>()
 		for ((from, to) in replacements) {
-			for (i in 0..(input.length - from.length)) {
-				if (input.indexOf(from, i) == i) {
-					val builder = StringBuilder(input)
-					builder.replace(i, i + from.length, to)
-					results += builder.toString()
-				}
+			for (index in input.allIndexesOf(from)) {
+				results += input.replace(index, index + from.length, to)
 			}
 		}
 		return results
@@ -54,53 +69,7 @@ class Day19 : AdventTask<Day19.Input, Int, Int>(2015, 19) {
 	}
 
 	override fun part2(input: Input): Int {
-		val initialMoleculeLength = input.initialMolecule.moleculeLength
-		val toCheck = LinkedList<Pair<String, Int>>()
-		val moleculeSet = mutableSetOf<String>()
-		toCheck += "e" to 0
-		moleculeSet += "e"
-
-		var leastSteps: Int? = null
-		var maxSteps = 0
-		while (!toCheck.isEmpty()) {
-			val (molecule, steps) = toCheck.removeFirst()
-			moleculeSet -= molecule
-			if (steps > 300) {
-				println("> Skipping")
-				continue
-			}
-			if (steps > maxSteps) {
-				println("> Got to maxSteps = $maxSteps")
-				maxSteps = steps
-			}
-			if (leastSteps != null && steps >= leastSteps)
-				continue
-			if (molecule.moleculeLength > initialMoleculeLength) {
-				println("> Too long molecule, skipping")
-				continue
-			}
-
-			if (molecule == input.initialMolecule) {
-				println("> Got it after $steps steps")
-				if (leastSteps == null || leastSteps > steps) {
-					leastSteps = steps
-					println(">> And it's the shortest one by far")
-				}
-				continue
-			}
-
-			getReplacements(molecule, input.replacements).forEach { newMolecule ->
-				if (newMolecule in moleculeSet) {
-					val existingToCheck = toCheck.first { it.first == newMolecule }
-					toCheck.remove(existingToCheck)
-				} else {
-					moleculeSet += newMolecule
-				}
-				toCheck += newMolecule to (steps + 1)
-			}
-		}
-
-		return leastSteps!!
+		return input.initialMolecule.moleculeLength - (input.initialMolecule.allIndexesOf("Rn").size + input.initialMolecule.allIndexesOf("Y").size) * 2 - 1
 	}
 
 	class Tests {
@@ -120,8 +89,8 @@ class Day19 : AdventTask<Day19.Input, Int, Int>(2015, 19) {
 
 		@TestFactory
 		fun part2(): Collection<DynamicTest> = createTestCases(
-				"HOH" expects 3,
-				"HOHOHO" expects 6
+				"HOH" expects 2,
+				"HOHOHO" expects 5
 		) { rawInput, expected ->
 			val baseInput = """
 				e => H
