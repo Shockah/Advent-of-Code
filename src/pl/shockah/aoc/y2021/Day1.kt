8658e8f2e7e5b1c7c8b3dea64ac9d8c3c8660615
Shockah <me@shockah.pl>
2021-12-01 09:28:03 +0100

    Y21D1

---
+++ src/pl/shockah/aoc/y2021/Day1.kt
@@ -0,0 +1,52 @@
+package pl.shockah.aoc.y2021
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import pl.shockah.aoc.AdventTask
+
+class Day1: AdventTask<List<Int>, Int, Int>(2021, 1) {
+	override fun parseInput(rawInput: String): List<Int> {
+		return rawInput.trim().lines().map { it.toInt() }
+	}
+
+	private fun task(input: List<Int>): Int {
+		return input.indices.toList().dropLast(1).map { input[it] < input[it + 1] }.count { it }
+	}
+
+	override fun part1(input: List<Int>): Int {
+		return task(input)
+	}
+
+	override fun part2(input: List<Int>): Int {
+		return task(input.windowed(3).map { it.sum() })
+	}
+
+	class Tests {
+		private val task = Day1()
+
+		private val rawInput = """
+			199
+			200
+			208
+			210
+			200
+			207
+			240
+			269
+			260
+			263
+		""".trimIndent()
+
+		@Test
+		fun part1() {
+			val input = task.parseInput(rawInput)
+			Assertions.assertEquals(7, task.part1(input))
+		}
+
+		@Test
+		fun part2() {
+			val input = task.parseInput(rawInput)
+			Assertions.assertEquals(5, task.part2(input))
+		}
+	}
+}
